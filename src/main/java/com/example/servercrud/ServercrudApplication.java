14c2b4dcaa32330092c8f80621574e0a119b929e
ATOME Ismael <atome@6-email>
2023-05-22 13:57:05 +0200

    first commit

---
+++ src/main/java/com/example/servercrud/ServercrudApplication.java
@@ -0,0 +1,12 @@
+package com.example.servercrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServercrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServercrudApplication.class, args);
+	}
+}
